b8342873fb7e5b2291202942ce70c38ebb9feb8a
keerthana <lnu.ke@northeastern.edu>
2023-04-20 22:35:18 -0400

    Publish repo

---
+++ RideSharer/MapViewController.swift
@@ -0,0 +1,49 @@
+//
+//  MapViewController.swift
+//  RideSharer
+//
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        checkLocationServices()
+    }
+    
+    
+    func checkLocationServices() {
+      if CLLocationManager.locationServicesEnabled() {
+        checkLocationAuthorization()
+      } else {
+        // Show alert letting the user know they have to turn this on.
+      }
+    }
+    
+    
+    func checkLocationAuthorization() {
+      switch CLLocationManager.authorizationStatus() {
+      case .authorizedWhenInUse:
+        mapView.showsUserLocation = true
+       case .denied: // Show alert telling users how to turn on permissions
+          let alert = UIAlertController(title: "Location Denied", message:"User have not provided permission", preferredStyle: UIAlertController.Style.alert)
+          alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+          self.present(alert, animated: true, completion: nil)
+       break
+      case .notDetermined:
+        locationManager.requestWhenInUseAuthorization()
+        mapView.showsUserLocation = true
+      case .restricted: // Show an alert letting them know what’s up
+       break
+      case .authorizedAlways:
+       break
+      }
+    }
+}
